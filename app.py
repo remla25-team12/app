6079d47d668dedb4ed47d79ed41be2b8ac982387
pengxuanHuang <pacers0406@gmail.com>
2025-05-17 14:43:31 +0200

    remove unwanted comments

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def people():
     return render_template("people.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000) # Change it back to 5000
+    app.run(debug=True, host="0.0.0.0", port=5000) 
 
